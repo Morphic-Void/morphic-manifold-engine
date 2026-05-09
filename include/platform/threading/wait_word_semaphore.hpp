d356052735a89a1c31d0cd2952432c3b431d0ea5
Ritchie Brannan <ritchie@morphicvoid.com>
2026-05-09 17:19:05 +0100

    Updating a section comment to align with the new class name

--- include/platform/threading/wait_word_semaphore.hpp
+++ include/platform/threading/wait_word_semaphore.hpp
@@ -43,7 +43,7 @@ namespace threading
 {
 
 //==============================================================================
-//  Counting semaphore
+//  Wait word based counting semaphore
 //==============================================================================
 
 class CWaitWordSemaphore
